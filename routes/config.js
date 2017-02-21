75c8ee0c32d4d9c12515a97146c6fda50bd9f78e
smedapati1 <smedapati@securustech.net>
2017-02-20 21:33:08 -0600

    updated callback url

--- routes/config.js
+++ routes/config.js
@@ -3,7 +3,7 @@
  */
 var config = {
   lob_api_key: 'xxxx',
-  instagram_redirect_uri: 'http://localhost:3000/api2',
+  instagram_redirect_uri: 'https://instagramhelper.herokuapp.com/api2',
   instagram_client_id: '1d6835e4c7254879b1cec9a89aa843b0',
   instagram_client_secret: '1536feb5da5e48199cb1cf553990324c'
 }
